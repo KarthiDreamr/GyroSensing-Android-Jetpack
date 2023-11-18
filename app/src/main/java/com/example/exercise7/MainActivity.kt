87e8240270c1b4843918ae4d030d443214025f74
KarthiDreamr <karthidreamr@gmail.com>
2023-11-18 09:18:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exercise7/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.exercise7
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.exercise7.ui.theme.Exercise7Theme
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Exercise7Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    GyroSensorValue()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Exercise7Theme {
+        Greeting()
+    }
+}
+
+
+@Composable
+fun GyroSensorValue() {
+    val context = LocalContext.current
+    val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+
+    val gyroValues = remember { mutableStateOf(Triple(0f, 0f, 0f)) }
+
+    val listener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+        override fun onSensorChanged(event: SensorEvent?) {
+            event?.let {
+                gyroValues.value = Triple(it.values[0], it.values[1], it.values[2])
+            }
+        }
+    }
+
+    sensorManager.registerListener(listener, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+    Column(modifier = Modifier.background(Color.LightGray).padding(16.dp)) {
+        Text(text = "Gyro Sensor Values", color = Color.Black)
+        IconRow("X: ${gyroValues.value.first}", Icons.Default.Refresh)
+        IconRow("Y: ${gyroValues.value.second}", Icons.Default.Refresh)
+        IconRow("Z: ${gyroValues.value.third}", Icons.Default.Refresh)
+    }
+}
+
+@Composable
+fun IconRow(text: String, icon: ImageVector) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(imageVector = icon, contentDescription = null)
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = text, color = Color.Black)
+    }
+}
